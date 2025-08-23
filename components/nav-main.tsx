940b58d8383542dd4110c4cae455e2c6ec7d77ac
Mansur Nurmukhambetov <funisinfinity26@gmail.com>
2025-08-23 21:34:08 +0500

    fix: update Quick Create link to point to the correct problems creation page

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -30,7 +30,7 @@ export function NavMain({
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
               asChild
             >
-              <a href="/dashboard/create">
+              <a href="/dashboard/problems/create">
                 <IconCirclePlusFilled />
                 <span>Quick Create</span>
               </a>
